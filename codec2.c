b1cc35d6a1693486344f5e84a670acd8ea93ff9a
shmooel28 <93682110+shmooel28@users.noreply.github.com>
2022-11-10 21:27:57 +0200

    Update codec2.c

--- codec2.c
+++ codec2.c
@@ -1,33 +1,62 @@
 #include <stdio.h>
 #include "codec.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#define RW_BLOCK 10
 
-int copyFiles(FILE* ptr1, FILE* ptr2){
-    int char1 , char2;
-    if (ptr1 && ptr2) {
-        while((char1=getc(ptr1))!= EOF)
-        {
-            fputc(char1, ptr2);
-        }
-    }
-    return 1;
-}
-
-int main(int argc, char *argv[]){
-    if(argc != 3){
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
         printf("You nedd exactly two files");
         exit(1);
     }
-    FILE * f1;
-    FILE* f2;
-    if((f1 = fopen(argv[1], "r")) ==NULL || (f2 = fopen (argv[2], "w")) ==NULL ){
-        printf("The file is not exsist");
+    int fsorce, fddst;
+    char *sourc = argv[1];
+    char *dest = argv[2];
+    printf("dest is %s:\n",dest);
+    if ((fsorce = open(sourc, O_RDONLY)) < 0)
+    {
+        perror(" cant open sorce file");
+        exit(-1);
+    }
+
+    if (fddst = open(dest, O_RDWR | O_CREAT) < 0)
+    {
+        perror(" cant open dest file");
         exit(1);
     }
-    if(copyFiles(f1, f2)){
-        printf("file is copied\n");
-    }
-    fclose(f1);
-    fclose(f2);
+
+    ssize_t readBytes, wroteBytes;
+    char *buf[10];
+    lseek(fddst,0,SEEK_SET);
+    do
+    {
+        readBytes = read(fsorce, buf, RW_BLOCK);
+        if (readBytes < 0)
+        {
+            printf("I/O errors\n");
+            exit(-10);
+        }
+
+        wroteBytes = write(fddst, buf, readBytes);
+        if (wroteBytes < RW_BLOCK)
+        {
+            printf("this is the error\n");
+            if (errno == EDQUOT)
+            {
+                printf("ERROR: out of quota.\n");
+            }
+            else if (errno == ENOSPC)
+            {
+                printf("ERROR: not enough disk space.\n");
+            }
+        }
+    } while (readBytes > 0);
+
+    close(fsorce);
+    close(fddst);
     return 0;
 }
